57d8ad190ae682c11e5f14cda22799cbfb8794e0
Isaac <i.cheng.ca@gmail.com>
2021-02-01 12:07:52 -0800

    Added num_round argument

--- bayesian_mcmc_rot_curve/run_mcmc.sh
+++ bayesian_mcmc_rot_curve/run_mcmc.sh
@@ -4,4 +4,4 @@ num_cores=10
 echo "Running round 0"
 python MCMC_w_dist_uncer.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
     --num_cores $num_cores --num_chains $num_cores --num_tune 2500 --num_iter 10000 \
-    --prior_set A1 --like_type sivia --filter_plx False
+    --num_samples 100 --prior_set A1 --like_type sivia --filter_plx False --num_round 0
