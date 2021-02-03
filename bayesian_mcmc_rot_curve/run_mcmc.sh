7d4c4c12f4b0a45bb2f1f4dd02c8cb8cccad1187
Isaac <i.cheng.ca@gmail.com>
2021-02-02 17:06:07 -0800

    changed --filter_plx syntax so it is more proper

--- bayesian_mcmc_rot_curve/run_mcmc.sh
+++ bayesian_mcmc_rot_curve/run_mcmc.sh
@@ -3,5 +3,9 @@ num_cores=10
 
 python MCMC_w_dist_uncer.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
     --num_cores $num_cores --num_chains $num_cores --num_tune 2500 --num_iter 5000 \
-    --num_samples 1000 --prior_set A1 --like_type sivia --filter_plx False \
+    --num_samples 1000 --prior_set A1 --like_type sivia \
     --num_rounds 1 --filter_method sigma
+
+# Other params:
+# --filter_plx (will also filter sources with e_plx/plx > 0.2)
+# --this_round (for setting starting point for filtering)
--- bayesian_mcmc_rot_curve/run_mcmc_local.sh
+++ bayesian_mcmc_rot_curve/run_mcmc_local.sh
@@ -1,4 +1,8 @@
 python MCMC_w_dist_uncer.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
     --num_cores 2 --num_chains 2 --num_tune 100 --num_iter 100 \
-    --num_samples 5 --prior_set A1 --like_type sivia --filter_plx False \
+    --num_samples 1000 --prior_set A1 --like_type sivia \
     --num_rounds 2 --filter_method sigma
+
+# Other params:
+# --filter_plx (will also filter sources with e_plx/plx > 0.2)
+# --this_round (for setting starting point for filtering)
