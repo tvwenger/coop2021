e5c0858040cb534dae7f60ad9a7b1479f5408c1c
Isaac <i.cheng.ca@gmail.com>
2021-02-03 13:56:34 -0800

    added cauchy PDF

--- bayesian_mcmc_rot_curve/run_mcmc_local.sh
+++ bayesian_mcmc_rot_curve/run_mcmc_local.sh
@@ -1,7 +1,7 @@
 python MCMC_w_dist_uncer.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
-    --num_cores 2 --num_chains 2 --num_tune 100 --num_iter 100 \
-    --num_samples 1000 --prior_set A1 --like_type sivia \
-    --num_rounds 2 --filter_method sigma
+    --num_cores 2 --num_chains 2 --num_tune 500 --num_iter 500 \
+    --num_samples 100 --prior_set A5 --like_type cauchy \
+    --num_rounds 2 --reject_method lnlike --filter_plx
 
 # Other params:
 # --filter_plx (will also filter sources with e_plx/plx > 0.2)
