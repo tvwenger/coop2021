84356971c37a6d89d332cf934a1c646ffd999e98
Isaac <i.cheng.ca@gmail.com>
2021-02-03 16:39:12 -0800

    sivia PDF

--- bayesian_mcmc_rot_curve/run_mcmc.sh
+++ bayesian_mcmc_rot_curve/run_mcmc.sh
@@ -3,8 +3,8 @@ num_cores=10
 
 python MCMC_w_dist_uncer.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
     --num_cores $num_cores --num_chains $num_cores --num_tune 2500 --num_iter 5000 \
-    --num_samples 100 --prior_set A1 --like_type cauchy \
-    --num_rounds 1 --reject_method lnlike --filter_plx
+    --num_samples 100 --prior_set A1 --like_type sivia \
+    --num_rounds 1 --reject_method sigma --filter_plx
 
 # Other params:
 # --filter_plx (will also filter sources with e_plx/plx > 0.2)
