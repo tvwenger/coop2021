99bb7cf0cb2853f23643d93cbd64bd85b73562d8
Isaac <i.cheng.ca@gmail.com>
2021-02-04 17:30:56 -0800

    Decreased number of tunings and iterations

--- bayesian_mcmc_rot_curve/run_mcmc.sh
+++ bayesian_mcmc_rot_curve/run_mcmc.sh
@@ -6,11 +6,12 @@ num_cores=10
 #     --num_samples 100 --prior_set A1 --like_type cauchy \
 #     --num_rounds 1 --reject_method sigma --filter_plx
 
+eacho "==== Testing simulated data ===="
 python MCMC_w_dist_uncer.py \
     /home/chengi/Documents/coop2021/bayesian_mcmc_rot_curve/mcmc_sim_data.pkl \
-    --num_cores $num_cores --num_chains $num_cores --num_tune 2000 --num_iter 5000 \
-    --num_samples 100 --prior_set A1 --like_type sivia \
-    --num_rounds 2 --reject_method sigma
+    --num_cores $num_cores --num_chains $num_cores --num_tune 1000 --num_iter 1000 \
+    --num_samples 100 --prior_set A1 --like_type cauchy \
+    --num_rounds 2 --reject_method lnlike
 
 # Other params:
 # --filter_plx (will also filter sources with e_plx/plx > 0.2)
