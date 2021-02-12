be5da9e19a867ce2a0efd2b36a3ad1dd4a354c72
Isaac <i.cheng.ca@gmail.com>
2021-02-12 09:45:10 -0800

    Now functioning kriging program

--- pec_motions/fit_pec_motions_krige.py
+++ pec_motions/fit_pec_motions_krige.py
@@ -13,7 +13,7 @@ import dill
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pykrige.kriging_tools as kt
-from pykrige.ok import OrdinaryKriging
+from pykrige.uk import UniversalKriging
 
 # Want to add my own programs as package:
 # Make a $PATH to coop2021 (twice parent folder of this file)
@@ -51,7 +51,7 @@ def main(prior_set, num_samples, num_rounds):
         free_roll = file["free_roll"]
 
     print(
-        "=== Fitting peculiar motions for "
+        "=== Fitting peculiar motions (KRIGING) for "
         f"({prior_set} priors & {num_rounds} MCMC rounds) ==="
     )
     print("Number of sources:", num_sources)
@@ -67,126 +67,144 @@ def main(prior_set, num_samples, num_rounds):
     x = x[y > -5]
     y = y[y > -5]
 
-    variogram_model = "linear"
-    vx_res_fit = OrdinaryKriging(
+    variogram_model = "gaussian"  # use "gaussian" or "spherical"
+    vx_res_fit = UniversalKriging(
         x,
         y,
         vx_res,
         variogram_model=variogram_model,
+        exact_values=False,
         # verbose=False,
-        # enable_plotting=False,
+        enable_plotting=True,
     )
-    vy_res_fit = OrdinaryKriging(
+    vy_res_fit = UniversalKriging(
         x,
         y,
         vy_res,
         variogram_model=variogram_model,
+        exact_values=False,
         # verbose=False,
-        # enable_plotting=False,
+        enable_plotting=True,
     )
+    print("Variogram Model:", variogram_model)
 
     # gridx, gridy = np.mgrid[-8:12:500j, -5:15:500j]
     gridx = np.linspace(-8, 12, 500)
     gridy = np.linspace(-5, 15, 500)
 
-    # # Radial basis function interpolation
-    # print("Rbf default epsilon (kpc):", Rbf(x, y, vx_res).epsilon)
-    # epsilon = 0.2  # kpc
-    # print("My epsilon (kpc):", epsilon)
+    # Interpolate
+    vx_res_interp, vx_res_interp_sigmasq = vx_res_fit.execute("grid", gridx, gridy)
+    vy_res_interp, vy_res_interp_sigmasq = vy_res_fit.execute("grid", gridx, gridy)
+    print("mean vx_res_interp:", np.mean(vx_res_interp))
+    print("min & max vx_res_interp:", np.min(vx_res_interp), np.max(vx_res_interp))
+    print("vx_res_interp mean sd:", np.mean(np.sqrt(vx_res_interp_sigmasq)))
+    print(
+        "vx_res_interp min & max sd:",
+        np.min(np.sqrt(vx_res_interp_sigmasq)),
+        np.max(np.sqrt(vx_res_interp_sigmasq)),
+    )
+    print()
+    print("mean vy_res_interp:", np.mean(vy_res_interp))
+    print("min & max vy_res_interp:", np.min(vy_res_interp), np.max(vy_res_interp))
+    print("vy_res_interp mean sd:", np.mean(np.sqrt(vy_res_interp_sigmasq)))
+    print(
+        "vy_res_interp min & max sd:",
+        np.min(np.sqrt(vy_res_interp_sigmasq)),
+        np.max(np.sqrt(vy_res_interp_sigmasq)),
+    )
 
-    # vx_res_fit = Rbf(x, y, vx_res, epsilon=epsilon)  # Residual x-components function
-    # vy_res_fit = Rbf(x, y, vy_res, epsilon=epsilon)  # Residual y-components function
+    print("vx_res_interp nan detected!") if np.sum(np.isnan(vx_res_interp)) else None
+    print("vy_res_interp nan detected!") if np.sum(np.isnan(vy_res_interp)) else None
+    print("vx_res_interp_sigmasq nan detected!") if np.sum(
+        np.isnan(vx_res_interp_sigmasq)
+    ) else None
+    print("vy_res_interp_sigmasq nan detected!") if np.sum(
+        np.isnan(vy_res_interp_sigmasq)
+    ) else None
+    print("=" * 6)
 
-    # # Interpolate values
-    # gridx, gridy = np.mgrid[-8:12:500j, -5:15:500j]
-    # vx_res_interp = vx_res_fit(gridx.flatten(), gridy.flatten())
-    # vx_res_interp = vx_res_interp.reshape(gridx.shape[0], gridy.shape[0])
-    # print()
-    # print("# vx_res_interp nans:", np.sum(np.isnan(vx_res_interp)))
-    # print("Mean vx_res_interp:", np.mean(vx_res_interp))
-    # print("min & max vx_res_interp:", np.nanmin(vx_res_interp), np.nanmax(vx_res_interp))
+    # Plot parameters
+    fig, ax = plt.subplots(1, 2, figsize=plt.figaspect(0.5))
+    # cmap = copy.copy(mpl.cm.get_cmap("viridis"))
+    # cmap.set_bad(color="black")
+    cmap = "viridis"
 
-    # vy_res_interp = vy_res_fit(gridx.flatten(), gridy.flatten())
-    # vy_res_interp = vy_res_interp.reshape(gridx.shape[0], gridy.shape[0])
-    # print("# vy_res_interp nans:", np.sum(np.isnan(vy_res_interp)))
-    # print("Mean vy_res_interp:", np.mean(vy_res_interp))
-    # print("min & max vy_res_interp:", np.nanmin(vy_res_interp), np.nanmax(vy_res_interp))
-    # print("=" * 6)
+    # Plot residual x-component
+    norm_x = mpl.colors.Normalize(
+        vmin=np.min([np.min(vx_res_interp), np.min(vx_res)]),
+        vmax=np.max([np.max(vx_res_interp), np.max(vx_res)]),
+    )
+    # norm_x = mpl.colors.Normalize(vmin=-150, vmax=150)
+    ax[0].imshow(vx_res_interp.T, origin="lower", extent=(-8, 12, -5, 15), norm=norm_x)
+    cbar_x = fig.colorbar(
+        mpl.cm.ScalarMappable(norm=norm_x, cmap=cmap), ax=ax[0], format="%.0f"
+    )
+    ax[0].axhline(y=0, linewidth=0.5, linestyle="--", color="k")  # horizontal line
+    ax[0].axvline(x=0, linewidth=0.5, linestyle="--", color="k")  # vertical line
+    ax[0].set_xlabel("x (kpc)")
+    ax[0].set_ylabel("y (kpc)")
+    ax[0].set_title("x-component")
+    cbar_x.ax.set_ylabel("Residual x-velocity", rotation=270)
+    cbar_x.ax.get_yaxis().labelpad = 15
+    ax[0].set_aspect("equal")
+    ax[0].grid(False)
 
-    # # Plot parameters
-    # fig, ax = plt.subplots(2, 1, figsize=plt.figaspect(2))
-    # cmap = "viridis"
+    # Plot residual y-components
+    norm_y = mpl.colors.Normalize(
+        vmin=np.min([np.min(vy_res_interp), np.min(vy_res)]),
+        vmax=np.max([np.max(vy_res_interp), np.max(vy_res)]),
+    )
+    # norm_y = mpl.colors.Normalize(vmin=-150, vmax=150)
+    ax[1].imshow(vy_res_interp.T, origin="lower", extent=(-8, 12, -5, 15), norm=norm_y)
+    cbar_y = fig.colorbar(
+        mpl.cm.ScalarMappable(norm=norm_y, cmap=cmap), ax=ax[1], format="%.0f"
+    )
+    ax[1].axhline(y=0, linewidth=0.5, linestyle="--", color="k")  # horizontal line
+    ax[1].axvline(x=0, linewidth=0.5, linestyle="--", color="k")  # vertical line
+    ax[1].set_xlabel("x (kpc)")
+    ax[1].set_ylabel("y (kpc)")
+    ax[1].set_title("y-component")
+    cbar_y.ax.set_ylabel("Residual y-velocity", rotation=270)
+    cbar_y.ax.get_yaxis().labelpad = 15
+    ax[1].set_aspect("equal")
+    ax[1].grid(False)
 
-    # # Plot residual x-component
-    # norm_x = mpl.colors.Normalize(
-    #     vmin=np.nanmin(vx_res_interp), vmax=np.nanmax(vx_res_interp)
-    # )
-    # ax[0].imshow(vx_res_interp.T, origin="lower", extent=(-8, 12, -5, 15), norm=norm_x)
-    # cbar_x = fig.colorbar(
-    #     mpl.cm.ScalarMappable(norm=norm_x, cmap=cmap), ax=ax[0], format="%.0f"
-    # )
-    # ax[0].axhline(y=0, linewidth=0.5, linestyle="--", color="k")  # horizontal line
-    # ax[0].axvline(x=0, linewidth=0.5, linestyle="--", color="k")  # vertical line
-    # ax[0].set_xlabel("x (kpc)")
-    # ax[0].set_ylabel("y (kpc)")
-    # ax[0].set_title("x-component")
-    # cbar_x.ax.set_ylabel("Residual x-velocity", rotation=270)
-    # cbar_x.ax.get_yaxis().labelpad = 20
-    # ax[0].set_aspect("equal")
-    # ax[0].grid(False)
+    # Plot actual residual motion data
+    ax[0].scatter(
+        x, y, c=vx_res, norm=norm_x, cmap="viridis", s=10, edgecolors="k", label="Masers"
+    )
+    ax[0].set_xlim(-8, 12)
+    ax[0].set_xticks([-5, 0, 5, 10])
+    ax[0].set_ylim(-5, 15)
+    ax[0].set_yticks([-5, 0, 5, 10, 15])
+    ax[0].legend(loc="lower left", fontsize=9)
+    ax[1].scatter(
+        x, y, c=vy_res, norm=norm_y, cmap="viridis", s=10, edgecolors="k", label="Masers"
+    )
+    ax[1].set_xlim(-8, 12)
+    ax[1].set_xticks([-5, 0, 5, 10])
+    ax[1].set_ylim(-5, 15)
+    ax[1].set_yticks([-5, 0, 5, 10, 15])
+    ax[1].legend(loc="lower left", fontsize=9)
 
-    # # Plot residual y-components
-    # norm_y = mpl.colors.Normalize(
-    #     vmin=np.nanmin(vy_res_interp), vmax=np.nanmax(vy_res_interp)
-    # )
-    # ax[1].imshow(vy_res_interp.T, origin="lower", extent=(-8, 12, -5, 15), norm=norm_y)
-    # cbar_y = fig.colorbar(
-    #     mpl.cm.ScalarMappable(norm=norm_y, cmap=cmap), ax=ax[1], format="%.0f"
-    # )
-    # ax[1].axhline(y=0, linewidth=0.5, linestyle="--", color="k")  # horizontal line
-    # ax[1].axvline(x=0, linewidth=0.5, linestyle="--", color="k")  # vertical line
-    # ax[1].set_xlabel("x (kpc)")
-    # ax[1].set_ylabel("y (kpc)")
-    # ax[1].set_title("y-component")
-    # cbar_y.ax.set_ylabel("Residual y-velocity", rotation=270)
-    # cbar_y.ax.get_yaxis().labelpad = 20
-    # ax[1].set_aspect("equal")
-    # ax[1].grid(False)
-
-    # # Plot actual residual motion data
-    # ax[0].scatter(
-    #     x, y, c=vx_res, norm=norm_x, cmap="viridis", s=10, edgecolors="k", label="Masers"
-    # )
-    # ax[0].set_xlim(-8, 12)
-    # ax[0].set_xticks([-5, 0, 5, 10])
-    # ax[0].set_ylim(-5, 15)
-    # ax[0].set_yticks([-5, 0, 5, 10, 15])
-    # ax[0].legend(loc="lower left", fontsize=9)
-    # ax[1].scatter(
-    #     x, y, c=vy_res, norm=norm_y, cmap="viridis", s=10, edgecolors="k", label="Masers"
-    # )
-    # ax[1].set_xlim(-8, 12)
-    # ax[1].set_xticks([-5, 0, 5, 10])
-    # ax[1].set_ylim(-5, 15)
-    # ax[1].set_yticks([-5, 0, 5, 10, 15])
-    # ax[1].legend(loc="lower left", fontsize=9)
-
-    # fig.suptitle(
-    #     f"Interpolated Peculiar Motions of {num_sources} Masers"
-    #     "\n(Radial Basis Function)"
-    # )
-    # # fig.suptitle("Interpolated Peculiar Motions of", len(x), "Masers")
-    # fig.tight_layout()
-    # filename = f"pec_mot_interp_{prior_set}_{num_samples}dist_{num_rounds}.jpg"
-    # fig.savefig(
-    #     Path(__file__).parent / filename, format="jpg", dpi=300, bbox_inches="tight",
-    # )
-    # plt.show()
+    fig.suptitle(
+        f"Interpolated Peculiar Motions of {num_sources} Masers\n"
+        fr"(Ordinary Kriging, \texttt{{variogram\_model={variogram_model}}})"
+    )
+    # fig.suptitle("Interpolated Peculiar Motions of", len(x), "Masers")
+    fig.tight_layout()
+    filename = f"pec_mot_interp_{prior_set}_{num_samples}dist_{num_rounds}_krige.jpg"
+    fig.savefig(
+        Path(__file__).parent / filename, format="jpg", dpi=300, bbox_inches="tight",
+    )
+    plt.show()
 
 
 if __name__ == "__main__":
-    prior_set_file = input("prior_set of file (A1, A5, B, C, D): ")
-    num_samples_file = int(input("Number of distance samples per source in file (int): "))
-    num_rounds_file = int(input("round number of file for best-fit parameters (int): "))
-
+    # prior_set_file = input("prior_set of file (A1, A5, B, C, D): ")
+    # num_samples_file = int(input("Number of distance samples per source in file (int): "))
+    # num_rounds_file = int(input("round number of file for best-fit parameters (int): "))
+    prior_set_file = "A5"
+    num_samples_file = 100
+    num_rounds_file = 5
     main(prior_set_file, num_samples_file, num_rounds_file)
