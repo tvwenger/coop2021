aa9accd488e0758a463f6ff47684627d9fb10f3e
Isaac <i.cheng.ca@gmail.com>
2021-02-15 13:18:29 -0800

    run_mcmc_w_fre_plx: increase num_tune & num_iter

--- bayesian_mcmc_rot_curve/run_mcmc_w_free_plx.sh
+++ bayesian_mcmc_rot_curve/run_mcmc_w_free_plx.sh
@@ -5,7 +5,7 @@ echo "==== MCMC w/ plx as model parameter ===="
 
 echo "=== Testing real data ==="
 python mcmc_w_free_plx.py /home/chengi/Documents/coop2021/data/hii_v2_20201203.db \
-    --num_cores $num_cores --num_chains $num_cores --num_tune 1000 --num_iter 1000 \
+    --num_cores $num_cores --num_chains $num_cores --num_tune 2000 --num_iter 10000 \
     --num_samples 1 --prior_set A1 --like_type cauchy \
     --num_rounds 3 --reject_method lnlike --free_Zsun --free_roll --auto_run
 
