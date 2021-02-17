2086e96a24574723848ed136f6474dfe0f948e22
Isaac <i.cheng.ca@gmail.com>
2021-02-16 16:57:29 -0800

    fixed typos

--- bayesian_mcmc_rot_curve/mcmc_w_free_plx_get_plx_stats.py
+++ bayesian_mcmc_rot_curve/mcmc_w_free_plx_get_plx_stats.py
@@ -13,7 +13,7 @@ import numpy as np
 
 infile = Path(
     "/home/chengi/Documents/coop2021/bayesian_mcmc_rot_curve/"
-    f"mcmc_outfile_A1_1dist_1.pkl"
+    f"mcmc_outfile_A1_1dist_2.pkl"
 )
 
 with open(infile, "rb") as f:
@@ -36,10 +36,34 @@ plx_diff = (plx_mcmc - plx_data) / e_plx_data
 # plx_diff = (plx_mcmc[rhat < 1.4] - plx_data[rhat < 1.4]) / e_plx_data [rhat < 1.4]
 e_plx_diff = (e_plx_mcmc - e_plx_data) / e_plx_data
 
-print("min & max abs plx diff (in termns of e_plx)", np.min(abs(plx_diff)), np.max(abs(plx_diff)))
-print("mean abs plx diff (in termns of e_plx)", np.mean(abs(plx_diff)))
-print("median abs plx diff (in termns of e_plx)", np.median(abs(plx_diff)))
-print("min & max e_plx diff (in termns of e_plx)", np.min(e_plx_diff), np.max(e_plx_diff))
-print("mean e_plx diff (in termns of e_plx)", np.mean(e_plx_diff))
-print("median e_plx diff (in termns of e_plx)", np.median(e_plx_diff))
-print("num MCMC e_plx > e_plx", np.sum(e_plx_diff > 0))
+print(
+    "min & max abs plx diff (in terms of database standard deviations)",
+    np.min(abs(plx_diff)),
+    np.max(abs(plx_diff)),
+)
+print(
+    "mean abs plx diff (in terms of database standard deviations)", np.mean(abs(plx_diff))
+)
+print(
+    "median abs plx diff (in terms of database standard deviations)",
+    np.median(abs(plx_diff)),
+)
+print("# MC parallax > 1 sigma from the database value:", np.sum(abs(plx_diff) > 1))
+print(
+    "\n=== Comparing MC parallax uncertainties to database uncertainties "
+    "to see if model constrains parallaxes further ==="
+)
+print(
+    "min & max e_plx diff (in terms of database e_plx)",
+    np.min(e_plx_diff),
+    np.max(e_plx_diff),
+)
+print("mean e_plx diff (in terms of database e_plx)", np.mean(e_plx_diff))
+print(
+    "median e_plx diff (in terms of database e_plx)", np.median(e_plx_diff)
+)
+print(
+    "# of MCMC parallaxes with uncertainties larger than corresponding database parallax:",
+    np.sum(e_plx_diff > 0),
+)
+
